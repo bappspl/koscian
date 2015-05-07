a8cc95e6fef12b1f5aa87522c06d4af1fa7ccc23
idik <idik91@github.com>
2015-05-07 18:46:03 +0200

    Zmiany w migracjach

--- data/migrations/7_cms_create_post.php
+++ data/migrations/7_cms_create_post.php
@@ -13,20 +13,17 @@ class CmsCreatePost extends AbstractMigration
              ->addColumn('status_id', 'integer')
              ->addColumn('category', 'string', array('null'=>true))
              ->addColumn('text', 'text')
+             ->addColumn('date_from', 'datetime', array('null'=>true))
+             ->addColumn('date_to', 'datetime', array('null'=>true))
+             ->addColumn('author_id', 'integer')
+             ->addColumn('filename_main', 'string', array('null'=>true))
              ->addForeignKey('status_id', 'cms_status', 'id', array('delete' => 'CASCADE', 'update' => 'NO_ACTION'))
+             ->addForeignKey('author_id', 'cms_users', 'id', array('delete' => 'CASCADE', 'update' => 'NO_ACTION'))
              ->save();
-
-        $this->table('cms_post_file', array())
-            ->addColumn('filename', 'string')
-            ->addColumn('post_id', 'integer')
-            ->addColumn('size', 'string')
-            ->addForeignKey('post_id', 'cms_post', 'id', array('delete' => 'CASCADE', 'update' => 'NO_ACTION'))
-            ->save();
     }
 
     public function down()
     {
         $this->dropTable('cms_post');
-        $this->dropTable('cms_post_file');
     }
 }
